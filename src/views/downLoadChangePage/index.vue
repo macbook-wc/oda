e2a3247b6ff457f8b1f2f21a5014340ed83f6b31
wangchuan <cw_2211589@163.com>
2024-02-21 18:21:55 +0800

    表单提交

--- src/views/downLoadChangePage/index.vue
+++ src/views/downLoadChangePage/index.vue
@@ -14,17 +14,25 @@
       </div>
       <!-- 循环产品 -->
       <div class="introduction" v-for="(item, index) in checkData">
-        <el-form :model="form" :key="index">
+        <el-form :model="form" :key="index" ref="formRef">
           <div v-for="(ite, ind) in item.labelList">
             <div class="mainTitle" :key="ind">{{ ite.productTitle }}</div>
             <el-form-item :label="ite.category" :key="ind">
-              <el-checkbox-group v-model="form[ind].labelList">
+              <el-checkbox-group
+                v-model="form[ind].labelList"
+                @change="handleCheckboxChange(ind)"
+              >
                 <div v-for="(it, i) in ite.labelList">
                   <el-checkbox :label="it" :name="it" :key="i" />
                 </div>
               </el-checkbox-group>
             </el-form-item>
-            <el-form-item label="选择时间" style="margin-left: 20px" :key="ind">
+            <el-form-item
+              label="选择时间"
+              style="margin-left: 20px"
+              :key="ind"
+              :rules="form[ind].dateRules"
+            >
               <div class="block">
                 <el-date-picker
                   v-model="form[ind].itemDate"
@@ -38,8 +46,8 @@
               </div>
             </el-form-item>
           </div>
-          <el-form-item>
-            <el-button type="primary" @click="submitForm">确认</el-button>
+          <el-form-item class="sub_btn">
+            <el-button type="primary" @click="submitForm(formRef)">确认</el-button>
           </el-form-item>
         </el-form>
       </div>
@@ -82,6 +90,7 @@
                 end-placeholder="End month"
                 value-format="YYYY-MM"
                 unlink-panels
+                :disabled="isDisable"
               />
             </div>
           </el-form-item>
@@ -192,22 +201,22 @@
           />
         </div>
       </el-form-item>
-    </el-form>
-    <template #footer>
-      <span class="dialog-footer">
+      <el-form-item> </el-form-item>
+      <div class="sub_btn">
         <el-button type="primary" class="btn_sub" @click="submit" size="large">
           确认
         </el-button>
-      </span>
-    </template>
+      </div>
+    </el-form>
   </el-dialog>
 </template>
 
 <script setup>
-import { getCheckData,submitDataRequest } from "../../utils/intefaceApi.js";
+import { getCheckData, submitDataRequest } from "../../utils/intefaceApi.js";
 let checkData = ref([]);
 const dialogFormVisible = ref(false);
 const isDisable = ref(true);
+const formRef = ref();
 const route = useRoute();
 
 const formData = reactive({
@@ -221,34 +230,77 @@ const formData = reactive({
 const focusDate = (datePicker) => {};
 
 const form = reactive([]);
-const form1 = reactive([]);
-
 onBeforeMount(() => {
   fetchData();
 });
 const submitForm = (formEl) => {
-  dialogFormVisible.value = true;
+  if (!formEl) return;
+  let i = 0;
+
+  let isNull = form.every((item, index) => {
+    return item.labelList.length == 0;
+  });
+  if (isNull) {
+    ElMessage({
+      message: "请选择要下载的文件！",
+      showClose: true,
+      type: "warning",
+      duration: 2000,
+    });
+    return;
+  }
+  while (i < form.length) {
+    console.log(form[i],"form[i].");
+    if (form[i].labelList.length > 0 && form[i].itemDate.length == 0) {
+      ElMessage({
+        message: `${form[i].category}类型下，请选择时间！`,
+        showClose: true,
+        type: "warning",
+        duration: 2000,
+      });
+      break;
+    } 
+    i++;
+  }
+  if(i == form.length){
+    dialogFormVisible.value = true;
+  }
 };
-async function  submit(){
+
+const handleCheckboxChange = (index) => {
+  form[index].dateRules = [
+    { required: form[index].labelList.length > 0, message: "请选择开始结束时间!" },
+  ];
+};
+
+async function submit() {
   let formFormate = [];
   form.forEach((item) => {
     let obj = {};
-    obj.itemStartDate = item.itemDate[0];
-    obj.itemEndDate = item.itemDate[1];
-    obj.labelList = item.labelList;
-    obj.category = item.category;
-    if( item.labelList.length>0){
+    if (item.labelList.length > 0) {
+      obj.itemStartDate = item.itemDate[0].replace("-", " ");
+      obj.itemEndDate = item.itemDate[1].replace("-", " ");
+      obj.labelList = item.labelList;
+      obj.category = item.category;
       formFormate.push(obj);
     }
   });
   const productId = route.query.productId;
   const productTitle = route.query.productTitle;
-  let odaProductDataVoIns = { productId, productTitle, categoryList: formFormate };
+  let odaProductDataVoIns = [{ productId, productTitle, categoryList: formFormate }];
   let userVoIn = formData;
   let odaDateRequestVoIn = { odaProductDataVoIns, userVoIn };
   let response = await submitDataRequest(odaDateRequestVoIn);
-  
-};
+  if (response.code == 200) {
+    dialogFormVisible.value = false;
+    ElMessage({
+      message: "已提交成功，请在邮箱查收！",
+      showClose: true,
+      type: "success",
+      duration: 2000,
+    });
+  }
+}
 async function fetchData() {
   try {
     let response = await getCheckData();
@@ -268,7 +320,6 @@ async function fetchData() {
         form.push(obj);
       });
     });
-    console.error("--form---", form);
   } catch (error) {
     console.error("An error occurred:", error);
     throw error;
@@ -281,6 +332,10 @@ const resetForm = (formEl) => {
 </script>
 
 <style lang="less" scoped>
+.sub_btn {
+  display: flex;
+  justify-content: center;
+}
 .check_content {
   display: flex;
   align-items: center;
